d750d7231d5abbafd331c0004c4088ca35e3181d
Darrell Pawson <dpawson905@gmail.com>
2020-10-16 10:31:50 -0400

    work on project page

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const { asyncErrorHandler} = require("../middleware");
 const projectController = require("../controllers/projectController");
 
-router.get('/', asyncErrorHandler(projectController.viewProjects));
-
-router.route('/:id')
-  .get(asyncErrorHandler(projectController.viewProject));
-
 router
   .route("/new-project")
   .get(asyncErrorHandler(projectController.indexPage))
   .post(asyncErrorHandler(projectController.postProject));
+
+router.get('/', asyncErrorHandler(projectController.viewProjects));
+
+router.route('/:id')
+  .get(asyncErrorHandler(projectController.viewProject));
 
 module.exports = router;
